f0007d9e3f01d37e496ea3119b20b245d8a9cf54
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2022-01-26 13:17:35 +0700

    fix: resolve dependencies from baileys v3 to v4 [Supported for multiple devices]

--- index.ts
+++ index.ts
@@ -1,76 +1,33 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
+import RBot from "./src/Rbot";
 config();
-import {RBot} from "./src/Rbot";
-import {enumCommand, FigletChalkStarter, Logger} from "./src/utils/logger"
-import {MongoConnect} from "./src/lib/MongoConnect";
-import * as Schema from "./src/Models";
-import {Message} from "./src/MessageHandler";
-import {RedisStore} from "./src/lib/Redis";
-import {RMessage} from "./src/type";
+// import {RBot} from "./src/Rbot";
+import { enumCommand, FigletChalkStarter, Logger } from "./src/utils/logger";
+import { MongoConnect } from "./src/lib/MongoConnect";
+// import * as Schema from "./src/Models";
+// import {Message} from "./src/MessageHandler";
+import { RedisStore } from "./src/lib/Redis";
+// import {RMessage} from "./src/type";
 
-async function Start(){
-    try {
-        FigletChalkStarter("RBOT");
+async function Start() {
+	try {
+		FigletChalkStarter("RBOT");
 
-        await MongoConnect(String(process.env.MONGO_URI))
+		await MongoConnect(String(process.env.MONGO_URI));
 
-        /** Depreceated */
-        // const redisClient = InitRedis();
-        // const redisClient = new RedisStore()
+		new RedisStore();
 
-        const rbot = new RBot();
-
-        // Find existing session
-        const existSession = await rbot.getSession(process.env.SESSION_ID || "R-Bot");
-        if(existSession) rbot.loadAuthInfo(existSession);
-
-        const msg = new Message(rbot);
-
-        rbot.on("open", () => {
-            rbot.updateSession(process.env.SESSION_ID || "R-Bot");
-            rbot.writeFileSession();
-        })
-
-        rbot.on("qr", (qr) => {
-            console.log(qr);
-            
-        })
-
-        rbot.on("chat-update", async (update) => {
-            if(!update.messages) return;
-            const {messages} = update;
-            const all = messages.all();
-
-            try{
-                const msg: RMessage = await rbot.messageParser(all[0])
-                const auth = await rbot.checkAuth(msg.sender.jid)
-
-                console.log(auth)
-                //
-                // const validatedMesssage = msg.validate(all[0]);
-                // if(!validatedMesssage) return;
-                // msg.msgHandler(validatedMesssage)
-            } catch (e) {
-                Logger.error(e)
-                // rbot.sendMessage(update)
-            }
-        })
-
-        // Run our server
-        await rbot.connect()
-
-    }catch (e) {
-        Logger.error(`Error ${e}`)
-        process.exit();
-    }
+		RBot();
+	} catch (e) {
+		Logger.error(`Error ${e}`);
+		process.exit();
+	}
 }
 
-Start().then(()=>{
-})
+Start().then(() => {});
 
-/**
- * Init DB (Mongo)
- * Init Redis
- * Init WA Client
- */
-
+// /**
+//  * Init DB (Mongo)
+//  * Init Redis
+//  * Init WA Client
+//  */
